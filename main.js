aa7d2c5540b28fccdcb6115ecfec8c9f335105a4
marylian <marylian@fullsail.edu>
2012-06-07 20:35:57 -0300

    Added function to that grabs the checkedbox values.

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 
 //Wait until the DOM is ready.
 window.addEventListener("DOMContentLoaded", function () {
-
 	//getElementByID Function
 	function getValue(x) {
 		var taskData = document.getElementById(x);
@@ -14,8 +13,8 @@ window.addEventListener("DOMContentLoaded", function () {
 		
 	function makeTask(){
 		var searchTag=document.getElementsByTagName("form"), 
-		selectedItem=getValue('selectItem'),
-		createItem=document.createElement('selectedItem');
+		select=getValue('selectItem'),
+		createItem=document.createElement('select');
 		createItem.setAttribute("id", "tasks");
 		for (var i=0, j=taskList.length; i<j; i++){
 			var initiateTask=document.createElement('task');
@@ -24,38 +23,48 @@ window.addEventListener("DOMContentLoaded", function () {
 			initiateTask.innerHTML=taskText;
 			createItem.appendChild(initiateTask);
 		}
-		selectedItem.appendChild(createItem);
+		select.appendChild(createItem);
 	}
+	
+	//Find which checkbox is checked
+	function getCheckedBox(){
+		var check=document.forms[0].weekday;
+		for(var i=0; i<check.length; i++){
+			if(check[i].checked){
+				dayValue=check[i].value;
+			}
+		}
+	}
+
 	//Set Link & Submit click events
 	function submitData(){
 		var id=Math.floor(Math.random()*53211245);
+		getCheckedBox();
 		var task={};
-//		task.weekday=["Day: ", dayValue];
+		task.weekday=["Day: ", dayValue];
 		task.subject=["Subject: ", getValue('sub').value];
 		task.period=["Period: ", getValue('period').value];
-		
 		task.age=["Age: ", getValue('age').value];
-		alert("Task has been saved");
-		localStorage.setItem(id, JSON.stringify(task));
 		task.tasks=["Tasks to be performed: ", getValue('tasks').value];
 		task.date=["Due date: ", getValue('duedate').value];
 		task.comments=["Your comments: ", getValue('comments').value];
-		
-			
+		alert("Task has been saved");
+		localStorage.setItem(id, JSON.stringify(task));
 	}
 	
-	/*function getData(){
+	function getData(){
 
 	}
 	
-	var displayLink=getValue('displayTask');
+	/* var displayLink=getValue('displayTask');
 	displayLink.addEventListener("click", getData);
 	
 	var clearLink=getValue('clearTask');
 	clearLink.addEventListener("click", clearData);*/
 	
 		//Variable defaults
-	var taskList=[" --Choose a Task-- ", "Create Tests", "Look for Project Ideas", "Complete Lesson Plans", "Grade Papers", "Complete Paperwork", "Contact Parents", "Buy School Supplies"];
+	var taskList=[" --Choose a Task-- ", "Create Tests", "Look for Project Ideas", "Complete Lesson Plans", "Grade Papers", "Complete Paperwork", "Contact Parents", "Buy School Supplies"],
+	weekdayValue;
 	makeTask(); //Calling the function that was created above.
 
 	var submitButton=getValue('tasksubmit');
